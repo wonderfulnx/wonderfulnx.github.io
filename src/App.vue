23a1b2d99761db48813c7294dc6ac9e5f949a6ec
wonderfulnx <naxinlegend@outlook.com>
2025-05-08 20:44:01 +0800

    update info.

--- src/App.vue
+++ src/App.vue
@@ -34,14 +34,14 @@ import TalkList from './components/TalkList.vue'
   <footer>
     <hr>
     <div class="row items-align-top">
-      <img class="col-1" src="./assets/footer-left.jpg" style="max-width: none;" alt="personal photo">
+      <img class="col-1" src="./assets/footer-avatar-3.jpg" style="width: 76px;" alt="personal photo">
       <div class="col" style="text-align: center; margin: auto;">
         <b>Relevant Links: </b>
         <a href="http://tns.thss.tsinghua.edu.cn/sun/" target="_blank">The <b>SUN</b> Group</a>
         | <a href="http://tns.thss.tsinghua.edu.cn/sun/members/YuanHe/" target="_blank">Prof. Yuan He</a>
-        <br><br>Updated by Xin Na in March 2025. 😉
+        <br><br>Updated by Xin Na in May 2025. 😉
       </div>
-      <img class="col-1" src="./assets/footer-right.jpg" style="max-width: none;" alt="personal photo">
+      <img class="col-1" src="./assets/footer-avatar-4.jpg" style="width: 76px;" alt="personal photo">
   </div>
   </footer>
 </template>
