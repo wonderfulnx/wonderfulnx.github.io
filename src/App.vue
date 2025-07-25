2ac1ac09fa758953a4e17f5b6e1901af5d9864d2
wonderfulnx <naxinlegend@outlook.com>
2025-07-25 18:54:57 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@ import TalkList from './components/TalkList.vue'
         <b>Relevant Links: </b>
         <a href="http://tns.thss.tsinghua.edu.cn/sun/" target="_blank">The <b>SUN</b> Group</a>
         | <a href="http://tns.thss.tsinghua.edu.cn/sun/members/YuanHe/" target="_blank">Prof. Yuan He</a>
-        <br><br>Updated by Xin Na in May 2025. 😉
+        <br><br>Updated by Xin Na in July 2025. 😉
       </div>
       <img class="col-1" src="./assets/footer-avatar-4.jpg" style="width: 76px;" alt="personal photo">
   </div>
